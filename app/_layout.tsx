2b8062eede381ef541b88e9a9475a01c9a53091a
IngridAkeida <ingridsanchesp@gmail.com>
2024-12-10 17:31:07 +0100

    Add About screen and update Drawer navigation with Home and About Us options

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,22 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer />
+      <Drawer>
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerLabel: 'Home',
+            title: 'overview',
+          }}
+        />
+        <Drawer.Screen
+          name="about"
+          options={{
+            drawerLabel: 'About Us',
+            title: 'overview',
+          }}
+        />
+      </Drawer>
     </GestureHandlerRootView>
   );
 }
---
+++ app/about.tsx
@@ -0,0 +1,17 @@
+import { SafeAreaView, StyleSheet, Text} from "react-native";
+
+export default function About() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>about</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+});
