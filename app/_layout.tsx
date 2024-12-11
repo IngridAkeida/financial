6103d82dcd592cb555831d2db31c40dfcc687e14
IngridAkeida <ingridsanchesp@gmail.com>
2024-12-11 13:02:37 +0100

    Enhance Drawer navigation with custom styles and icons for Home and About Us screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,29 @@
 import { Drawer } from "expo-router/drawer";
+import { View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
+      <Drawer screenOptions={{
+        drawerPosition: 'left', //default is left but you can change it to right
+        drawerType: 'front', //default is front but you can change it to back, slide or permanent
+        drawerStyle: {
+          backgroundColor: 'white',
+          width: 280,
+        },
+        drawerActiveTintColor: '#a700a7',
+        drawerInactiveTintColor: '#420042',
+        drawerContentStyle: {
+          backgroundColor: '#fae5fa',
+        },
+      }}>
         <Drawer.Screen
           name="index"
           options={{
             drawerLabel: 'Home',
             title: 'overview',
+            drawerIcon: ({color, size}) => <View style={{width: size, height: size, backgroundColor:color, borderRadius:20}}></View>,
           }}
         />
         <Drawer.Screen
@@ -17,6 +31,7 @@ export default function RootLayout() {
           options={{
             drawerLabel: 'About Us',
             title: 'overview',
+            drawerIcon: ({color, size}) => <View style={{width: size, height: size, backgroundColor:color, borderRadius:20}}></View>,
           }}
         />
       </Drawer>
